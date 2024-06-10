8f36b5feb773e8b3508abd73287f14cd779f5373
AlphaT7 <7742643+AlphaT7@users.noreply.github.com>
2024-06-10 10:28:40 -0400

    added sound effects for terminal typing; fixed inconsistent media query issue

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,15 +1,36 @@
-async function typeIt(elId, text) {
+import { Howl } from "howler";
+let userInteraction = false;
+
+async function typeIt(elId, text, sound) {
   for (let char of text) {
     document.getElementById(elId).innerHTML += char;
-    await new Promise((resolve) => setTimeout(resolve, 75));
+    sound.play();
+    let time = Math.floor(Math.random() * 60) + 16;
+    await new Promise((resolve) => setTimeout(resolve, time));
   }
 }
 
-typeIt(
-  "mobileDescription",
-  "Self-taught web developer, casual gamer, husband and father of 4"
-);
-typeIt(
-  "desktopDescription",
-  "Self-taught web developer, casual gamer, husband and father of 4"
-);
+document.addEventListener("pointerup", (e) => {
+  if (userInteraction) return;
+
+  function play() {
+    let text =
+      "Self-taught web developer, casual gamer, husband and father of 4";
+    let mediaQuery = window.matchMedia("(max-width: 700px)").matches;
+
+    if (mediaQuery) {
+      typeIt("mobileDescription", text, sound);
+    } else {
+      typeIt("desktopDescription", text, sound);
+    }
+
+    userInteraction = true;
+  }
+
+  const sound = new Howl({
+    src: ["sound/typeEffect.mp3"],
+    onload: () => {
+      play();
+    },
+  });
+});
