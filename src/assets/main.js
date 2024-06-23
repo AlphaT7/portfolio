0234765b7c3677a3cc3adf502d067b4ff4cf10bb
AlphaT7 <7742643+AlphaT7@users.noreply.github.com>
2024-06-22 20:33:00 -0400

    removed typing and sound effect; added svg/tsparticles polygon mask effect in footer

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,36 +1,15 @@
-import { Howl } from "howler";
-let userInteraction = false;
+import { loadAll } from "@tsparticles/all";
 
-async function typeIt(elId, text, sound) {
-  for (let char of text) {
-    document.getElementById(elId).innerHTML += char;
-    sound.play();
-    let time = Math.floor(Math.random() * 60) + 16;
-    await new Promise((resolve) => setTimeout(resolve, time));
-  }
-}
+loadAll(tsParticles);
 
-document.addEventListener("pointerup", (e) => {
-  if (userInteraction) return;
-
-  function play() {
-    let text =
-      "Self-taught web developer, casual gamer, husband and father of 4";
-    let mediaQuery = window.matchMedia("(max-width: 700px)").matches;
-
-    if (mediaQuery) {
-      typeIt("mobileDescription", text, sound);
-    } else {
-      typeIt("desktopDescription", text, sound);
-    }
-
-    userInteraction = true;
-  }
-
-  const sound = new Howl({
-    src: ["sound/typeEffect.mp3"],
-    onload: () => {
-      play();
-    },
+tsParticles
+  .load({
+    id: "tsparticles",
+    url: "./config.polygon.json",
+  })
+  .then((container) => {
+    console.log("callback - tsparticles config loaded");
+  })
+  .catch((error) => {
+    console.error(error);
   });
-});
