937265aedd588bde66fc0cf023738a81ae4eb0a1
AlphaT7 <7742643+AlphaT7@users.noreply.github.com>
2024-06-23 13:20:45 -0400

    tweaked particle density and duration; put it in a cycle to restore the animation every 30 seconds

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,14 +2,21 @@ import { loadAll } from "@tsparticles/all";
 
 loadAll(tsParticles);
 
-tsParticles
-  .load({
-    id: "tsparticles",
-    url: "./config.polygon.json",
-  })
-  .then((container) => {
-    console.log("callback - tsparticles config loaded");
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+function animateSVG() {
+  tsParticles
+    .load({
+      id: "tsparticles",
+      url: "./config.polygon.json",
+    })
+    .then((container) => {
+      console.log("callback - tsparticles config loaded");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  setTimeout(() => {
+    animateSVG();
+  }, 30000);
+}
+
+animateSVG();
